89ad6068a388a1994d90323085b05cbd6463ddd3
Samuel Holland <samuel@sholland.org>
2017-01-29 02:36:38 -0600

    add example build configuration

---
+++ config.mk
@@ -0,0 +1,17 @@
+# set to a non-empty string to enable debugging
+#DEBUG :=
+
+# suffix for binary files (e.g. .exe on windows)
+#SUFFIX :=
+
+# the C compiler to use
+#CC := clang
+
+# flags to provide to the C compiler
+#CFLAGS := -fno-common -march=native -O2 -pipe -Wa,--noexecstack
+
+# flags to provide to the C preprocessor
+#CPPFLAGS := 
+
+# flags to provide to the linker
+#LDFLAGS := -Wl,--hash-style=both -Wl,-O1 -Wl,-z,noexecstack -Wl,-z,now
