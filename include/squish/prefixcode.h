950d004283793bfef2e819a51ef6888c45302b8a
Samuel Holland <samuel@sholland.org>
2017-02-09 02:36:38 -0600

    add huffman_compress() and prefix_encode()

--- include/squish/prefixcode.h
+++ include/squish/prefixcode.h
@@ -28,6 +28,29 @@ struct prefixcode;
  */
 long huffman_code(struct distribution *dist, struct prefixcode **code);
 
+/**
+ * Compress a buffer using a Huffman code. The output buffer will include the codebook.
+ * @param buffer The data to compress
+ * @param size The size of the uncompressed data in bytes
+ * @param output A pointer wher the location of the output buffer will be stored on success.
+ *				 This is a double pointer!
+ * @param outsize An integer where the size of the output buffer will be stored on success.
+ * @return 0 if the operation was successful, or a negative value representing an error
+ */
+long huffman_compress(void *buffer, size_t size, void *output, size_t *outsize);
+
+/**
+ * Compress a buffer using the given prefix code. The output buffer must already be allocated.
+ * @param code The prefix code to use
+ * @param buffer A buffer containing uncompressed data
+ * @param size The size of the uncompressed data
+ * @param output A buffer to fill with compressed data and the Huffman table
+ * @param outsize The size of the output buffer
+ * @return The number of bytes written to the output, or a negative value representing an error
+ */
+long prefix_encode(struct prefixcode *code, void *buffer, size_t size, void *output,
+                   size_t outsize);
+
 #ifdef __cplusplus
 }
 #endif
--- lib/prefixcode.c
+++ lib/prefixcode.c
@@ -7,10 +7,13 @@
 #include <errno.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <internal/heap.h>
 #include <internal/utils.h>
 #include <squish/prefixcode.h>
+
+#define TABLE_SIZE (ALPHABET_SIZE * sizeof(uint32_t))
 
 struct huffman_node {
 	struct huffman_node *left;  /**< The left child of this node (0 for leaf nodes). */
@@ -58,8 +61,8 @@ huffman_code(struct distribution *dist, struct prefixcode **code)
 		status = -errno;
 		goto out;
 	}
-	/* Copy the distribution to the nodes that will form the heap. Ensure each symbol in the alphabet
-	 * has a nonzero probability, or combining nodes would result in an infinite loop of 0+0 = 0.
+	/* Copy the distribution to the nodes that will form the heap. Each symbol in the alphabet must
+	 * have a nonzero probability, or combining nodes would result in an infinite loop of 0 + 0 = 0.
 	 * While this does skew the distribution, the difference will be negligible for all but the
 	 * smallest of data sets. */
 	for (size_t i = 0; i < ALPHABET_SIZE; i += 1) {
@@ -119,16 +122,63 @@ huffman_code(struct distribution *dist, struct prefixcode **code)
 
 	/* Send the created codebook back to the caller. */
 	*code = newcode;
-	/* Avoid freeing the codebook. */
-	newcode = 0;
 
 out:
 	if (heap)
 		heap_free(heap);
-	if (newcode)
+	if (status < 0 && newcode)
 		free(nodes);
 	if (nodes)
 		free(nodes);
+	return status;
+}
+
+long
+huffman_compress(void *buffer, size_t size, void *output, size_t *outsize)
+{
+	long status = 0;
+	size_t osize;
+	unsigned char *obuffer = 0;
+	struct distribution *dist = 0;
+	struct prefixcode *code = 0;
+
+	if (!buffer || !size || !output || !outsize)
+		return -EINVAL;
+
+	if (!(dist = distribution_new()))
+		goto out;
+	if ((status = buffer_distribution(dist, buffer, size)) < 0)
+		goto out;
+	if ((status = huffman_code(dist, &code)) < 0)
+		goto out;
+	/* Make room for storing the table with the compressed data. Size is in bits. */
+	osize = TABLE_SIZE * 8;
+	/* Calculate the size of the compressed data. */
+	for (size_t i = 0; i < ALPHABET_SIZE; i += 1)
+		osize += dist->counts[i] * log2u32(code->words[i]);
+	/* Convert output size to bytes. Round up. */
+	osize = (osize + CHAR_BIT - 1) / CHAR_BIT;
+
+	/* Allocate the output buffer. */
+	if (!(obuffer = calloc(1, osize)))
+		goto out;
+	/* Copy the Huffman table. That the type size matches the reserved size is not an accident. */
+	memcpy(obuffer, code->words, TABLE_SIZE);
+	/* Encode the data. */
+	if ((status = prefix_encode(code, buffer, size, obuffer + TABLE_SIZE, osize - TABLE_SIZE)) < 0)
+		goto out;
+
+	/* Send back our result. */
+	*(void **)output = obuffer;
+	*outsize = osize;
+
+out:
+	if (code)
+		free(code);
+	if (dist)
+		distribution_free(dist);
+	if (status < 0 && obuffer)
+		free(obuffer);
 	return status;
 }
 
@@ -161,4 +211,64 @@ huffman_traverse(struct huffman_node *node, unsigned int path, struct prefixcode
 	}
 
 	return status;
+}
+
+long
+prefix_encode(struct prefixcode *code, void *buffer, size_t size, void *output, size_t outsize)
+{
+	size_t byte = 0;
+	unsigned char *inbuf = buffer, *outbuf = output;
+	unsigned int bit = 0;
+
+	if (!code || !buffer || !size || !output || !outsize)
+		return -EINVAL;
+
+	for (size_t i = 0; i < size; i += 1) {
+		uint32_t word = code->words[inbuf[i]];
+		unsigned int wordlen = log2u32(word);
+
+		/* Prepare the word by shifting it to the far left (throwing away the leading 1). */
+		word <<= 32 - wordlen;
+
+		/* First, fill any remaining bits in the current byte. */
+		if (bit) {
+			/* Find the number of available bits left in this byte. */
+			unsigned int todo = CHAR_BIT - bit;
+			/* This should leave the first 'todo' bits of the word in the lowest 'todo' bits. */
+			unsigned char tmp = word >> (32 - todo);
+			/* Set those bits in the current output byte. */
+			outbuf[byte] |= tmp;
+			if (wordlen >= todo) {
+				/* This filled the byte. Move on to the next byte. */
+				bit = 0;
+				byte += 1;
+				/* There may be bits left to write. Shift out the already-written ones. */
+				word <<= todo;
+				wordlen -= todo;
+			} else {
+				/* This wrote the whole word, and this byte still has available bits in it. */
+				bit += wordlen;
+				wordlen = 0;
+			}
+		}
+
+		/* Fill out more bytes as needed, and as space allows. */
+		while (wordlen) {
+			if (byte >= outsize)
+				return -ENOSPC;
+			/* Set this output byte to the high byte of the code word. */
+			outbuf[byte] = word >> 24;
+			if (wordlen >= 8) {
+				/* This was a full byte. bit is still 0. */
+				byte += 1;
+				wordlen -= 8;
+			} else {
+				/* This was the last byte, and it was a partial byte. */
+				bit += wordlen;
+				wordlen = 0;
+			}
+		}
+	}
+
+	return 0;
 }
